94938d7b046bc72f25446abb764530f27e865591
suyashsonawane <suyashsonawane007@gmail.com>
2020-11-25 15:29:03 +0530

    paths added

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def logout():
     return home()
 
 
-if __name__ == "__main__":
-    import os
-    app.secret_key = os.urandom(12)
-    port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     import os
+#     app.secret_key = os.urandom(12)
+#     port = int(os.environ.get('PORT', 33507))
+#     app.run(host='0.0.0.0', port=port)
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+import app
+import os
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run()
