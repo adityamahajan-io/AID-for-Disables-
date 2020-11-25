e569f7427d59debd5f0e9cbf79452b870aae0e60
suyashsonawane <suyashsonawane007@gmail.com>
2020-11-25 15:19:09 +0530

    paths added

--- app.py
+++ app.py
@@ -51,5 +51,7 @@ def logout():
 
 
 if __name__ == "__main__":
+    import os
     app.secret_key = os.urandom(12)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host='0.0.0.0', port=port)
